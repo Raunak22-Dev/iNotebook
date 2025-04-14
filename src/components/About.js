c54d8406d65789ed8dfa827a774d3ae7b9e68723
Raunak Gangwal <raunakgangwal22@gmail.com>
2025-04-15 00:23:18 +0530

    some little changes

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2 className='mt-5'>About iNoteBook</h2>
-      <p>iNotebook is a sleek and intuitive note-taking web application designed to help users efficiently capture, organize, and manage their thoughts, ideas, and important information. With a user-friendly interface and modern features, iNotebook allows you to create, edit, and categorize notes based on title, description, and tags. The app ensures seamless user experience with functionalities like real-time updates, secure login/logout, and easy navigation. Whether you're jotting down quick reminders or managing detailed notes, iNotebook is a reliable and powerful tool for all your note-taking needs.</p>
+    <div className="container mt-5">
+      <h2>About iNoteBook</h2>
+      <p>
+        iNotebook is a sleek and intuitive note-taking web application designed to help users efficiently capture, organize, and manage their thoughts, ideas, and important information. With a user-friendly interface and modern features, iNotebook allows you to create, edit, and categorize notes based on title, description, and tags. The app ensures seamless user experience with functionalities like real-time updates, secure login/logout, and easy navigation. Whether you're jotting down quick reminders or managing detailed notes, iNotebook is a reliable and powerful tool for all your note-taking needs.
+      </p>
 
-The source code for iNotebook is available on  
-<a href="https://github.com/Raunak22-Dev"> GitHub
-</a>
-. Feel free to explore, contribute, and improve it!
+      <p>
+        The source code for iNotebook is available on&nbsp;
+        <a href="https://github.com/Raunak22-Dev" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        . Feel free to explore, contribute, and improve it!
+      </p>
+
+      <div className="text-end mt-4">
+        <h6 className="text-muted">----Made by Raunak Manoj Gangwal</h6>
+      </div>
     </div>
   )
 }
